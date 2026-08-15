6f7dfa536e4092d1a3f7b7c891831c1c75069716
Bhavyashree15 <159897991+Bhavyashree15@users.noreply.github.com>
2026-08-15 22:34:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
-  experimental: {
-    serverActions: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
